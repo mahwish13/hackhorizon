a59c4c3d87517083d8ebbf8f9428772132b83b88
Noor1805 <kashafnoor346@gmail.com>
2026-04-11 00:24:27 +0530

    error solved

--- server/models/User.js
+++ server/models/User.js
@@ -50,16 +50,11 @@ const userSchema = new mongoose.Schema({
 });
 
 // Pre-save hook for password hashing
-userSchema.pre('save', async function(next) {
-    if (!this.password || !this.isModified('password')) return next();
+userSchema.pre('save', async function() {
+    if (!this.password || !this.isModified('password')) return;
     
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (err) {
-        next(err);
-    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Instance method to compare password
